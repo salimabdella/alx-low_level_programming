df932acd2fe5e4394a0e8c0b3baa79d4a7331358
masterSal <salimabdellanuru@gmail.com>
2021-08-26 23:55:05 +0300

    Q.8

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ *
+ * Return: must return 0 but in this case 1
+ */
+int main(void)
+{
+	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
+}
+
