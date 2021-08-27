f59c59ec5025c33bde054c1aa7d4c2e8ed1fabf8
masterSal <salimabdellanuru@gmail.com>
2021-08-27 20:19:05 +0300

    Q.9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ *
+ * Return:0
+ */
+int main(void)
+{
+	int i;
+
+	i = 48;
+	while (i < 58)
+	{
+		putchar(i);
+		if (i != 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		i++;
+	}
+	return (0);
+}
