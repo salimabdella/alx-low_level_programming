2b1ae52b13c4922ecdbf184f5126ea7fed6486f8
masterSal <salimabdellanuru@gmail.com>
2021-08-27 19:35:48 +0300

    Q.3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ *
+ * Return:0
+ */
+int main(void)
+{
+        int i;
+
+        i = 97;
+        while (i < 123)
+        {
+                putchar(i);
+                i++;
+        }
+	for (i=65; i<91; i++)
+	{
+		putchar(i);
+	}
+        putchar('\n');
+        return (0);
+}
