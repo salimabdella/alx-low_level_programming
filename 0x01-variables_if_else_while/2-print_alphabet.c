e7a9155742c459da2b83789c2eb04080d739e235
masterSal <salimabdellanuru@gmail.com>
2021-08-27 19:30:17 +0300

    Q.2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/* betty style doc for function main goes there */
+/**
+ * main - main functions
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	
+	int i;
+	for (i=97; i<123; i++)
+	{
+		putchar((char)i);
+	}
+	putchar('\n');
+
+	return (0);
+}
+
