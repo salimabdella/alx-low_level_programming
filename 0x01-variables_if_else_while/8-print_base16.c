7a1c1508679c9e22a1da03fd8f99c97c06372d8b
masterSal <salimabdellanuru@gmail.com>
2021-08-27 20:07:58 +0300

    Q.8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ *
+ * Return:0
+ */
+int main(void)
+{
+	int i;
+
+	i = 48;
+	while (i < 64)
+	{
+		if (i < 58)
+		{
+			putchar(i);
+		}
+		else
+		{
+			putchar(i+7);
+		}
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
+
