f81f5d0eedddb554ad28308e48b6d1703dbafec7
masterSal <salimabdellanuru@gmail.com>
2021-08-27 19:42:44 +0300

    Q.4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ *
+ * Return:0
+ */
+int main(void)
+{
+	int i;
+	i = 97; 
+	while (i < 123)
+	{
+		if (i != 101 && i != 113)
+		{
+			putchar(i);
+		}
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
+
