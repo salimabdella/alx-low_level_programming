d7e83a6e1f08b9bef7ca973cb62fb977e3f5c732
masterSal <salimabdellanuru@gmail.com>
2021-09-01 11:17:16 +0300

    Q.0 and README.md

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,17 @@
+/**
+ * main - main function
+ * Return: 0
+ */
+
+int main(void) {
+	int i = 0;
+	char c[] = "Holberton\n";
+
+	while (i < 10)
+	{
+		_putchar(c[i]);
+		i++;
+	}
+	return (0);
+}
+
