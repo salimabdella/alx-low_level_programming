b2ccf2b32c432e4cedf1f70da604e671668c6010
masterSal <salimabdellanuru@gmail.com>
2021-09-06 20:07:45 +0300

    fix Q.4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,21 @@
+/**
+ * print_rev - print in reverse
+ * Description: print string in reverse
+ * @s: string to reverse
+ *
+ * Return: void
+ */
+void print_rev(char *s)
+{
+	int i = 0;
+
+	while (*(s + i))
+		i++;
+	i = i - 1;
+	while (i >= 0)
+	{
+		_putchar(*(s + i));
+		i--;
+	}
+	_putchar('\n');
+}
