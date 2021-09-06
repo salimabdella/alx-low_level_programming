a4f31b2de9a6f260100153ed5454e1c50f4a8696
masterSal <salimabdellanuru@gmail.com>
2021-09-06 19:21:45 +0300

    Q.1

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+/**
+ * swap_int - swap numbers
+ * Description: swaps given two numbers
+ * @a: first number to swap
+ * @b: second number to swap
+ *
+ * Return: void
+ */
+void swap_int(int *a, int *b) {
+	const int oldAVal = *a;
+	*a = *b;
+	*b = oldAVal;
+}
+
