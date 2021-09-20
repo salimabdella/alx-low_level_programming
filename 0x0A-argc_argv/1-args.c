c8d8c701fd24055ec33cf9d7a4e6ac0a020b0400
masterSal <salimabdellanuru@gmail.com>
2021-09-20 19:37:04 +0300

    Q.1

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -8,6 +8,15 @@
  */
 int main(int argc, char *argv[])
 {
-	printf("%s\n", argv[argc-1]);
+	int i;
+
+	if (argc == 1)
+		printf("%d\n", argc - 1);
+	else
+	{
+		for (i=0; *argv; i++, argv++)
+			;
+		printf("%d\n", i - 1);
+	}
 	return (0);
 }
