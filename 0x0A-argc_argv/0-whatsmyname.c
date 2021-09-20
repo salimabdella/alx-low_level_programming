4c6a771cfe2eb48f1ee2a2329eb400c4e704bac9
masterSal <salimabdellanuru@gmail.com>
2021-09-20 19:26:53 +0300

    fix Q.0

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -2,10 +2,13 @@
 
 /**
  * main - main
+ * @argc: command line args count
+ * @argv: array of command line args
+ *
  * Return: Always 0.
  */
 int main(int argc, char * argv[])
 {
-	printf("%s", argv[argc-1]);
+	printf("%s\n", argv[argc-1]);
 	return (0);
 }
