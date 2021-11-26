56b5cdf8706c88ff8eac07de37f5ee2103d6cccf
masterSal <salimabdellanuru@gmail.com>
2021-11-26 15:29:18 +0300

    Q.11 fix

--- 0x17-doubly_linked_lists/103-keygen.c
+++ 0x17-doubly_linked_lists/103-keygen.c
@@ -8,7 +8,7 @@ int f2(const char*, int);
 int f3(const char*, int);
 int f4(const char*, int);
 int f5(const char*, int);
-int f6(void);
+int f6(const char*);
 
 
 /**
@@ -18,101 +18,101 @@ int f6(void);
  *
  * Return: 0 on success, -1 on error
  */
-int main(int argc, char** argv[])
+int main(int argc, char* argv[])
 {
-	if (argc != 3)
+	char* weird_str = "A-CHRDw87lNS0E9B2TibgpnMVys5XzvtOGJcYLU+4mjW6fxqZeF3Qa1rPhdKIouk";
+	const char * username;
+	int username_length;
+	char key_user[7];
+
+	if (argc != 2)
 	{
-		printf("%s <key> <username>\n", argv[0]);
+		printf("%s <username>\n", argv[0]);
 		return (-1);
 	}
 
-	const char* weird_str = "A-CHRDw87lNS0E9B2TibgpnMVys5XzvtOGJcYLU+4mjW6fxqZeF3Qa1rPhdKIouk";
-	const char * key = (char*)argv[1];
-	const char * username = (char*)argv[2];
+	username = (char*)argv[1];
+	username_length = strlen(username);
 
-	const int key_len = strlen(key);
-	char key_user[7];
-
-	// check username char
+	/* check username char */
 	if (strlen(username) != 6)
 	{
 		printf("[-] username length must be 6");
 		return (-1);
 	}
 
-	// computer the first function
-	key_user[0] = weird_str[f1(key_len)];
-	key_user[1] = weird_str[f2(key, key_len)];
-	key_user[2] = weird_str[f3(key, key_len)];
-	key_user[3] = weird_str[f4(key, key_len)];
-	key_user[4] = weird_str[f5(key, key_len)];
-	key_user[5] = weird_str[f6()];
+	key_user[0] = weird_str[f1(username_length)];
+	key_user[1] = weird_str[f2(username, username_length)];
+	key_user[2] = weird_str[f3(username, username_length)];
+	key_user[3] = weird_str[f4(username, username_length)];
+	key_user[4] = weird_str[f5(username, username_length)];
+	key_user[5] = weird_str[f6(username)];
 
-	printf("--> key: %s\n--> username: %s\n", key, key_user);
+	printf("%s", key_user);
 	return (0);
 }
 
 
 /**
  * f1 - generate the 1st index for the key
- * @key_length - the key length
+ * @username_length - the username length
  *
  * Return: int
  */
-int f1(int key_length)
+int f1(int username_length)
 {
-	return (key_length ^ 0x3B) & 0x3F;
+	return (username_length ^ 0x3B) & 0x3F;
 }
 
 /**
  * f2 - generate the 2nd index for the key
- * @key - the key
- * @key_length - the key length
+ * @username - username
+ * @username_length - username length
  *
  * Return: int
  */
-int f2(const char* key, int key_length)
+int f2(const char* username, int username_length)
 {
 	int i;
 	int counter = 0;
-	for ( i = 0; i < key_length; ++i )
-		counter += (int) key[i];
+	for ( i = 0; i < username_length; ++i )
+		counter += (int) username[i];
 	return ((counter ^ 0x4F) & 0x3F);
 }
 
 
 /**
  * f3 - generate the 3rd index for the key
- * @key - the key
- * @key_length - the key length
+ * @username - username
+ * @username_length - username length
  *
  * Return: int
  */
-int f3(const char* key, int key_length)
+int f3(const char* username, int username_length)
 {
 	int i;
-	char counter = 0;
-	for ( i = 0; i < key_length; ++i )
-		counter *= key[i];
-	return ( ((int)counter ^ 0x55) & 0x3F);
+	int counter = 1;
+	for ( i = 0; i < username_length; ++i )
+		counter *= username[i];
+	return ( (counter ^ 0x55) & 0x3F);
 }
 
 
 /**
  * f5 - generate the 4th index for the key
- * @key - the key
- * @key_length - the key length
+ * @username - the key
+ * @username_length - the username length
  *
  * Return: int
  */
-int f4(const char* key, int key_length)
+int f4(const char* username, int username_length)
 {
 	int i;
-	int counter = *key;
-	for ( i = 0; i < key_length; ++i )
+	int counter = *username;
+	for ( i = 0; i < username_length; ++i )
 	{
-		if ( key[i] > counter )
-			counter = key[i];
+		if ( username[i] > counter )
+			counter = username[i];
 	}
 	srand(counter ^ 0xE);
 	return (rand() & 0x3F);
@@ -121,31 +121,32 @@ int f4(const char* key, int key_length)
 
 /**
  * f5 - generate the 5th index for the key
- * @key - the key
- * @key_length - the key length
+ * @username - username
+ * @username_length - the username length
  *
  * Return: int
  */
-int f5(const char* key, int key_length)
+int f5(const char* username, int username_length)
 {
 	int i;
 	int counter = 0;
-	for ( i = 0; i < key_length; ++i )
-		counter += (key[i] * key[i]);
+	for ( i = 0; i < username_length; ++i )
+		counter += ((int) username[i] * (int) username[i]);
 	return ((counter ^ 0xEF) & 0x3F);
 }
 
 
 /**
  * f6 - generate the 6th index for the key
+ * @username - username
  *
  * Return: int
  */
-int f6(void)
+int f6(const char* username)
 {
 	int i;
 	int counter = 0;
-	for ( i =0; i < 0x74; ++i )
+	for ( i =0; i < username[0]; ++i )
 		counter = rand();
 	return ((counter ^ 0xE5) & 0x3F);
 }
